b352225cd86754b6309bbb158a7d90f2b200c85c
gerard singian <gerard.singian@outlook.com>
2022-02-11 09:19:18 +0800

    Control Box Enhancement o Adding new Raw Materials

--- COMPLETE_FLAT_UI/Forms/Dry Warehouse/Add Modal/frmAddNewItemModal.Designer.cs
+++ COMPLETE_FLAT_UI/Forms/Dry Warehouse/Add Modal/frmAddNewItemModal.Designer.cs
@@ -347,7 +347,7 @@ namespace ULTRAMAVERICK.Forms.Dry_Warehouse
             this.txtcreatedBy.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtcreatedBy.Enabled = false;
             this.txtcreatedBy.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtcreatedBy.Location = new System.Drawing.Point(36, 396);
+            this.txtcreatedBy.Location = new System.Drawing.Point(36, 413);
             this.txtcreatedBy.Name = "txtcreatedBy";
             this.txtcreatedBy.Size = new System.Drawing.Size(145, 20);
             this.txtcreatedBy.TabIndex = 473;
@@ -378,7 +378,7 @@ namespace ULTRAMAVERICK.Forms.Dry_Warehouse
             this.txtcreatedAt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtcreatedAt.Enabled = false;
             this.txtcreatedAt.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtcreatedAt.Location = new System.Drawing.Point(211, 396);
+            this.txtcreatedAt.Location = new System.Drawing.Point(211, 413);
             this.txtcreatedAt.Name = "txtcreatedAt";
             this.txtcreatedAt.Size = new System.Drawing.Size(145, 20);
             this.txtcreatedAt.TabIndex = 475;
@@ -396,6 +396,8 @@ namespace ULTRAMAVERICK.Forms.Dry_Warehouse
             this.Controls.Add(this.materialCard1);
             this.Controls.Add(this.textBox1);
             this.Margin = new System.Windows.Forms.Padding(1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "frmAddNewItemModal";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Add New Raw Material";
