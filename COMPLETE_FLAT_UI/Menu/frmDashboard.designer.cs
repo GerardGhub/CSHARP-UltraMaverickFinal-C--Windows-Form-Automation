99af5e43417ff6d0a471fa5552ff72dcc7966224
gerard singian <gerard.singian@outlook.com>
2022-03-02 09:32:02 +0800

    Sizable false and the dashboard

--- COMPLETE_FLAT_UI/Menu/frmDashboard.designer.cs
+++ COMPLETE_FLAT_UI/Menu/frmDashboard.designer.cs
@@ -429,6 +429,7 @@
             this.Font = new System.Drawing.Font("Segoe UI Light", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.MinimizeBox = false;
             this.Name = "FormLogo";
+            this.Sizable = false;
             this.Text = "DASHBOARD";
             this.Load += new System.EventHandler(this.FormLogo_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
