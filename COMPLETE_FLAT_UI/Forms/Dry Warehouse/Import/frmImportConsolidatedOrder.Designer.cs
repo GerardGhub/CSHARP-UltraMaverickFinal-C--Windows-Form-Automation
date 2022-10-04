20b60cfb2f23d59e6a14317a69b7bd22953a6292
gerard singian <gerard.singian@outlook.com>
2022-10-04 10:41:45 +0800

    Import Store Order Add Order Id column == Done

--- COMPLETE_FLAT_UI/Forms/Dry Warehouse/Import/frmImportConsolidatedOrder.Designer.cs
+++ COMPLETE_FLAT_UI/Forms/Dry Warehouse/Import/frmImportConsolidatedOrder.Designer.cs
@@ -498,11 +498,11 @@ namespace ULTRAMAVERICK.Forms.Dry_Warehouse.Import
             // 
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.Color.Transparent;
-            this.label4.Font = new System.Drawing.Font("Segoe UI Light", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Font = new System.Drawing.Font("Roboto", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label4.Location = new System.Drawing.Point(23, 73);
+            this.label4.Location = new System.Drawing.Point(24, 72);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.Size = new System.Drawing.Size(46, 15);
             this.label4.TabIndex = 610;
             this.label4.Text = "Sheet :";
             // 
