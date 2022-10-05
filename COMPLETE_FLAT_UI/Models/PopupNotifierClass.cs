9171bed33dbe88ad55086598cf76f62a650381a5
gerard singian <gerard.singian@outlook.com>
2022-10-05 09:53:36 +0800

    Invalid Excel Template

--- COMPLETE_FLAT_UI/Models/PopupNotifierClass.cs
+++ COMPLETE_FLAT_UI/Models/PopupNotifierClass.cs
@@ -219,7 +219,7 @@ namespace ULTRAMAVERICK.Models
             popup.TitleColor = Color.White;
             popup.TitlePadding = new Padding(255, 7, 0, 0);
             popup.TitleFont = new Font("Tahoma", 10);
-            popup.ContentText = "Invalid Document some data is not matched on the requested file ...";
+            popup.ContentText = "Invalid Excel Template";
             popup.ContentColor = Color.White;
             popup.ContentFont = new System.Drawing.Font("Tahoma", 11F);
             popup.Size = new Size(350, 100);
